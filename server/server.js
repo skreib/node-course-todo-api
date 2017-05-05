41ae748efd1c5315708a27bb52e0e6560bea3c8e
Roman Vitruk <roman.vitruk@pdffiller.com>
2017-05-05 22:21:12 +0300

    Add GET  /todos route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,14 @@ app.post('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   })
+});
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.listen(3000, () => {
